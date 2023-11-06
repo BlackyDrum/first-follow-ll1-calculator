a38a7273e53ca7acd9df15ed6b15f888e5100092
BlackyDrum <ganiaytan1010@web.de>
2023-11-06 22:30:42 +0100

    Add comments

--- src/index.js
+++ src/index.js
@@ -46,19 +46,21 @@ document.getElementById('run_btn').addEventListener('click', () => {
     terminals = [...new Set(terminals)];
     symbols = [...new Set(symbols)];
 
+    // Calculate first sets
     let fi_string = "";
     variables.forEach(variable => {
         fi_string += `FI(${variable}) = { ${removeDuplicateWords(removeLastOccurrence(calculateFirstSets(variable), ",")).trim()} }\n`;
         follow_trace = [];
     })
 
+    // Calculate follow sets
     let fo_string = "";
     variables.forEach(variable => {
-        //debugger;
         fo_string += `FO(${variable}) = { ${removeLastOccurrence(calculateFollowSets(variable), ",").trim()} }\n`;
         follow_trace = [];
     })
 
+    // Calculate lookahead sets
     let la_string = "";
     lines.forEach((line, index) => {
         let la = removeDuplicateWords(removeLastOccurrence(calculateLookaheadSets(index), ","));
@@ -68,6 +70,7 @@ document.getElementById('run_btn').addEventListener('click', () => {
         follow_trace = [];
     })
 
+    // Check if grammar is LL(1)
     let ll1_string = analyzeLL1();
 
     let output = document.getElementById('output_area');
@@ -76,6 +79,8 @@ document.getElementById('run_btn').addEventListener('click', () => {
 })
 
 function analyzeLL1() {
+    // Intersect every rule with the same variable.
+    // If the result set contains no elements, continue, otherwise terminate with error message
     for (let i = 0; i < lookaheads.length; i++) {
         for (let j = 0; j < lookaheads.length; j++) {
             if (i !== j && lookaheads[i].variable === lookaheads[j].variable) {
@@ -98,26 +103,39 @@ function calculateFollowSets(variable) {
     lines.forEach(line => {
         const rule = line.split(' ');
         for (let i = 2; i < rule.length; i++) {
+            // If there is a terminal one position right from the variable and the current follow set includes epsilon,
+            // include that terminal in the follow set and delete epsilon
             if (i + 1 < rule.length && follows.includes(EPSILON) && terminals.includes(rule[i + 1])) {
                 follows += rule[i + 1] + ", ";
                 follows = replaceAll(follows, EPSILON, "");
             }
+            // If there is a variable one position right from the current variable and the current follow set includes epsilon,
+            // calculate the first set of the variable and include that first set in the follow set for the current variable
+            // and delete epsilon
             else if (i + 1 < rule.length && follows.includes(EPSILON) && variables.includes(rule[i + 1])) {
                 follows = replaceAll(follows, EPSILON, "");
                 follows += calculateFirstSets(rule[i + 1]);
             }
+            // If there is neither a terminal nor a variable one position right from the current variable, and the current
+            // follow set includes epsilon, calculate the follow set of the left side variable and include that follow set in the follow
+            // set for the current variable and delete epsilon
             else if (i + 1 >= rule.length && follows.includes(EPSILON)) {
                 follows = replaceAll(follows, EPSILON, "");
                 if (rule[0] !== variable)
                     follows += calculateFollowSets(rule[0]);
             }
             if (rule[i] === variable) {
+                // If there is a terminal one position right from the variable, include that terminal in the follow set
                 if (i + 1 < rule.length && terminals.includes(rule[i + 1])) {
                     follows += rule[i + 1] + ", ";
                 }
+                // If there is a variable one position right from the current variable, calculate the first set of the variable
+                // and include that first set in the follow set for the current variable
                 else if (i + 1 < rule.length && variables.includes(rule[i + 1])) {
                     follows += calculateFirstSets(rule[i + 1]);
                 }
+                // If there is neither a terminal nor a variable one position right from the current variable, calculate
+                // the follow set of the left side variable and include that follow set in the follow set for the current variable
                 else if (i + 1 >= rule.length && rule[0] !== variable && !follow_trace.includes(rule[0])) {
                     follow_trace.push(rule[0]);
                     follows += calculateFollowSets(rule[0]);
@@ -138,20 +156,26 @@ function calculateFirstSets(variable) {
         const rule = line.split(' ');
         if (rule[0] === variable) {
             for (let i = 2; i < rule.length; i++) {
+                // If we reach epsilon, terminate because we are finished
                 if (i > 2 && terminate && !firsts.includes(EPSILON)) return firsts;
+                // If we reach an epsilon, delete that and continue
                 if (i > 2 && firsts.includes(EPSILON)) {
                     firsts = replaceAll(firsts, EPSILON, "");
                 }
+                // If we reach a terminal, include that in the first set and terminate
                 if (terminals.includes(rule[i])) {
                     if (!firsts.includes(rule[i])) {
                         firsts += rule[i] + ", ";
                     }
                     break;
                 }
+                // If we reach a variable, calculate the first set of that variable and
+                // include that in the first set for the current variable
                 else if (variables.includes(rule[i])) {
                     firsts += calculateFirstSets(rule[i]);
                     if (firsts !== "") terminate = true;
                 }
+                // If we reach an epsilon, include that in the first set and return
                 else if (rule[2] === EPSILON) {
                     firsts += EPSILON + " ";
                     return firsts;
@@ -169,15 +193,21 @@ function calculateLookaheadSets(index) {
 
     const rule = lines.split(' ');
     for (let i = 2; i < rule.length; i++) {
+        // If we reach an epsilon, calculate the follow set of the left side variable
+        // and include that in the lookahead set for the current rule
         if (rule[2] === EPSILON) {
             lookaheads += calculateFollowSets(rule[0]);
             break;
         }
 
+        // If we reach a terminal, include that in the lookahead set for the current rule
+        // and terminate
         if (terminals.includes(rule[i])) {
             lookaheads += rule[i] + ", ";
             break;
         }
+        // If we reach a variable, calculate the first set of the variable.
+        // If there is an epsilon, continue, otherwise terminate
         else if (variables.includes(rule[i])) {
             lookaheads += calculateFirstSets(rule[i]);
             if (!lookaheads.includes(EPSILON)) {
